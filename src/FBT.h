d082c5dc15ae3e08a9e39aee1b05abb49a0fbec5
Eduardo Ramirez <ejramire@uc.cl>
2012-05-26 17:45:38 -0400

    tiempo: 0.144

--- src/FBT.h
+++ src/FBT.h
@@ -8,22 +8,48 @@
 #ifndef FBT_H_
 #define FBT_H_
 
-#include "List.h"
+#include <math.h>
 
 namespace std {
 
 class FBT {
 public:
-	FBT();
-	virtual ~FBT();
-	void clearFbt(int nivel); //Hasta que nivel se limpia
-	int ball(int nivel); //Hasta que nivel recorre la bola
-	int process(int nivel,int repeticiones); //Hasta que nivel se recorre y cuantas repeticiones tendra la bola
-	void printTree();
-	Lista<string>* split(string lin, char ch);
-private:
-	bool* nodes;
-	int* niveles;//en el indice de nivel-1 indica hasta que elemento del array corresponde
+	int process(int nivel,int repeticiones){
+		int* binario = new int[nivel-1];
+		repeticiones--;
+		decToBin(repeticiones, binario,nivel);
+		return (pow(2, nivel-1) + binToDec(binario,nivel));
+		
+	}
+	
+	void decToBin(int number, int* bin, int nivel){
+		int potencia = pow(2, nivel - 2);
+		int indice = 0;
+		while (indice<nivel-1) {
+			if (number>=potencia) {
+				bin[indice] = 1;
+				number-=potencia;
+			}else{
+				bin[indice]=0;
+			}
+				
+				potencia=potencia/2;
+				indice ++;
+		}
+	}
+	
+	int binToDec(int* bin, int nivel){
+		int potencia, resultado;
+		potencia = 1;
+		resultado = 0;
+		for (int i = 0; i<nivel -1; i++) {
+			resultado += bin[i]*potencia;
+			potencia = potencia*2;
+		}
+		return resultado;
+	}
+
+
 };
 
 } /* namespace std */
--- src/Tarea4.2.cpp
+++ src/Tarea4.2.cpp
@@ -6,48 +6,34 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "FBT.h"
+
+#include <math.h>
 
 using namespace std;
 
 int main() {
 
-
-	string numberOfTest;
-	getline(cin, numberOfTest);
-	int numberOfT = atoi(numberOfTest.c_str());
-	string* listOfTest = new string[numberOfT];
-	string input_line = "";
-
-	for(int i = 0; i < numberOfT;i++){
-		getline(cin, input_line);
-		listOfTest[i] = input_line;
-	}
-	getline(cin, input_line);
-
-	FBT* fbt = new FBT();
-
+	FBT fbt;
+	int result;
 	
-	for(int i = 0; i < numberOfT;i++){
-		Lista<string>* lista = fbt->split(listOfTest[i],' ');
-		int nivel = atoi(lista->get(0).c_str());
-		int repeticiones =atoi(lista->get(1).c_str());
-		cout<<fbt->process(nivel,repeticiones)<<endl;
-	}
+    int N;
+    while( scanf( "%d", &N ) != EOF )
+    {
+        int i;
+        for( i = 0 ; i < N ; i++ )
+        {
+            int D, I;
+            scanf( "%d%d", &D, &I );
+        
+		result = fbt.process(D, I); 
+            printf( "%d\n", result);
+        }
+		return 0;
+    }
 
-	/*int test = fbt->process(4,2);
-	int test2 = fbt->process(3,4);
-	int test3= fbt->process(10,1);
-	int test4 = fbt->process(2,2);
-	int test5 = fbt->process(8,12800);
-
-	cout << test << endl;
-	cout << test2 << endl;
-	cout << test3 << endl;
-	cout << test4 << endl;
-	cout << test5 << endl;*/
 	return 0;
 }
